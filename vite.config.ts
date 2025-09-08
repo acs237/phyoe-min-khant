aad67e8a43e0fc1771b97446b13f8b50579cf8a4
acs237 <acs23072000@gmail.com>
2025-09-08 18:36:27 +1000

    deploy config codes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
- base: "/pmk-personal-portfolio/",
+ base: "/pmk-personal-portfolio",
  plugins: [react(), tailwindcss()],
  preview: {
   port: 3000,
