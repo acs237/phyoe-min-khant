4b2d5478d5cfae90862e2f40469b05e04a0b6398
acs237 <acs23072000@gmail.com>
2025-09-09 10:05:29 +1000

    full link in a href in homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-import PageList from './PageList';
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import PersonalJourneyPage from './pages/PersonalJourney';
 
 function App() {
   return (
     <Router>
-      <PageList />
+    <Routes>
+      <Route path='/pmk-personal-portfolio' element={<HomePage />} />
+      <Route path='/pmk-personal-portfolio/personal-journey' element={<PersonalJourneyPage />} />
+      <Route path='/pmk-personal-portfolio/vlog' element={<div>Vlog Page</div>} />
+      <Route path='/pmk-personal-portfolio/coaching' element={<div>Coaching Page</div>} />
+      <Route path='/pmk-personal-portfolio/about-me' element={<div>About Me Page</div>} />
+    </Routes>
     </Router>
+    
   )
 }
 
