10fb773fd69690e90a17ba553fdd300e9fad61ad
Phyoe-Min-Khant <phyoeminkhant99.pmk@gmail.com>
2025-09-13 19:28:21 +1000

    Update portfolio.ts

--- src/assets/content/portfolio.ts
+++ src/assets/content/portfolio.ts
@@ -8,59 +8,52 @@ export interface PortfolioCardProps {
 
 export const portfolios: PortfolioCardProps[] = [
     {
-      title: "My Personal Journey",
+      title: "3 Storey Residence in Pinlon",
       address: "Yangon, Myanmar",
-      date: "2024",
-      image: "images/stonehenge.jpeg",
-      copyright: "2024 Phyoe Min Khant. All rights reserved."
+      date: "2023",
+      image: "images/p1.png",
+      copyright: "Image credit - K n A Architects Group"
     },
     {
-      title: "Professional Experience",
+      title: "2.5 Storey Residence - Rose Park",
       address: "Yangon, Myanmar",
-      date: "2024",
-      image: "images/stonehenge.jpeg",
-      copyright: "2024 Phyoe Min Khant. All rights reserved."
+      date: "2023",
+      image: "images/p2.png",
+      copyright: "Image credit - K n A Architects Group"
     },
     {
-      title: "Core Skills & Expertise",
+      title: "3 Storey Residence in North Dagon",
       address: "Yangon, Myanmar",
-      date: "2024",
-      image: "images/stonehenge.jpeg",
-      copyright: "2024 Phyoe Min Khant. All rights reserved."
+      date: "2023",
+      image: "images/p3.png",
+      copyright: ""
     },
     {
-      title: "My Vlog & Content",
-      address: "Yangon, Myanmar",
-      date: "2024",
-      image: "images/stonehenge.jpeg",
-      copyright: "2024 Phyoe Min Khant. All rights reserved."
+      title: "2 Storey Medium House - Hantharwaddy Housing Estate",
+      address: "Bago, Myanmar",
+      date: "2023",
+      image: "images/p4.png",
+      copyright: "Image credit - ANANDA D & D"
     },
     {
-      title: "Vision & Goals",
-      address: "Yangon, Myanmar",
-      date: "2024",
-      image: "images/stonehenge.jpeg",
-      copyright: "2024 Phyoe Min Khant. All rights reserved."
+      title: "2 Storey Medium House - Hantharwaddy Housing Estate",
+      address: "Bago, Myanmar",
+      date: "2023",
+      image: "images/p5.png",
+      copyright: "Image credit - DESIGN K"
     },
     {
-      title: "Achievements & Recognition",
-      address: "Yangon, Myanmar",
-      date: "2024",
-      image: "images/stonehenge.jpeg",
-      copyright: "2024 Phyoe Min Khant. All rights reserved."
+      title: "2 Storey Medium House - Hantharwaddy Housing Estate",
+      address: "Bago, Myanmar",
+      date: "2023",
+      image: "images/p6.png",
+      copyright: "Image credit - DESIGN K"
     },
     {
-      title: "Leadership Philosophy",
-      address: "Yangon, Myanmar",
-      date: "2024",
-      image: "images/stonehenge.jpeg",
-      copyright: "2024 Phyoe Min Khant. All rights reserved."
-    },
-    {
-      title: "Innovation & Future",
-      address: "Yangon, Myanmar",
-      date: "2024",
-      image: "images/stonehenge.jpeg",
-      copyright: "2024 Phyoe Min Khant. All rights reserved."
+      title: "1 Storey Medium House - Hantharwaddy Housing Estate",
+      address: "Bago, Myanmar",
+      date: "2023",
+      image: "images/p7.png",
+      copyright: "Image credit - DESIGN K"
     }
   ];
