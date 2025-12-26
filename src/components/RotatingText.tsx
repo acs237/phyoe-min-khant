5c0b9533a6162af0f7d2df71b19a7b294a075299
acs237 <acs23072000@gmail.com>
2025-12-26 17:20:12 +1000

    manage underneath line position in Insights from My Journey component

--- src/components/RotatingText.tsx
+++ src/components/RotatingText.tsx
@@ -17,7 +17,7 @@ export default function RotatingText({sentences}: RotatingTextProps) {
   }, [sentences.length]);
 
   return (
-    <div className="h-40 md:h-20 flex align-center">
+    <div className="flex items-center h-50 md:h-20">
       <AnimatePresence mode="wait">
         <motion.h1
           key={sentences[index]}
